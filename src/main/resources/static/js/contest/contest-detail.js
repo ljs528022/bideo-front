5cf751b7664eb39950721ace18378119b5d71104
JUNSU\dlwns <dlwnstn0315@gmail.com>
2026-03-31 15:11:42 +0900

    update

--- src/main/resources/static/js/contest/contest-detail.js
+++
@@ -1,9 +0,0 @@
-function toggleScrap(btn) {
-    btn.classList.toggle("active");
-}
-
-function copyLink() {
-    navigator.clipboard.writeText(window.location.href).then(function () {
-        alert("링크가 복사되었습니다.");
-    });
-}
--- src/main/resources/static/js/contest/contest-register.js
+++
@@ -1,5 +0,0 @@
-const startdate = document.getElementById("startDate").value;
-console.log(date);
-
-const enddate = document.getElementById("endDate").value;
-console.log(date);
